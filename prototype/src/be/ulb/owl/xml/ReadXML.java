dbf78d13d9c10a4ccd44b7b98a8ed9e97d12d69a
Detobel36 <detobel36@phoenix-rebirth.fr>
2016-11-07 14:28:30 +0100

    Add library

--- prototype/src/be/ulb/owl/xml/ReadXML.java
+++ prototype/src/be/ulb/owl/xml/ReadXML.java
@@ -5,29 +5,35 @@
  */
 package be.ulb.owl.xml;
 
+import org.jdom2.input.SAXBuilder;
+
+
 /**
  *
  * @author Detobel36
  */
 public class ReadXML {
     
-    // Example
-    //Ajouter cette méthodes à la classe JDOM2
-//    static void afficheALL() {
-//        //On crée une List contenant tous les noeuds "etudiant" de l'Element racine
-//        List listEtudiants = racine.getChildren("etudiant");
+//    Example
+//      On crée une instance de SAXBuilder
+//      SAXBuilder sxb = new SAXBuilder();
+//      try
+//      {
+//         //On crée un nouveau document JDOM avec en argument le fichier XML
+//         //Le parsing est terminé ;)
+//         document = sxb.build(new File("Exercice2.xml"));
+//      }
+//      catch(Exception e){}
 //
-//        //On crée un Iterator sur notre liste
-//        Iterator i = listEtudiants.iterator();
-//        while(i.hasNext()) {
-//            //On recrée l'Element courant à chaque tour de boucle afin de
-//            //pouvoir utiliser les méthodes propres aux Element comme :
-//            //sélectionner un nœud fils, modifier du texte, etc...
-//            Element courant = (Element)i.next();
-//            //On affiche le nom de l’élément courant
-//            System.out.println(courant.getChild("nom").getText());
-//        }
-//    }
+//      //On initialise un nouvel élément racine avec l'élément racine du document.
+//      racine = document.getRootElement();
     
+    
+    public static void loadXML(String filename) {
+        
+        SAXBuilder sxb = new SAXBuilder();
+        
+        
+    }
     
 }
