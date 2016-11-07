bdab4a0f2d999eccb68ee826d0de104255bab65c
Detobel36 <detobel36@phoenix-rebirth.fr>
2016-11-07 14:10:33 +0100

    Add README Add empty file for read XML

---
+++ prototype/src/README.md
@@ -0,0 +1,17 @@
+# OpenWifiLocalizator
+
+Rémy Detobel - *000408013*
+Denis Hoornaert - *000413326*
+Nathan Licardo - *000408278*
+Robin Petit - *000408282*
+
+
+## Convention
+```
+public void foo(Object param) {
+    // Code
+}
+```
+**CamelCase**      
+Example Class name:
+`ExampleClass`
---
+++ prototype/src/be/ulb/owl/xml/ReadXML.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.ulb.owl.xml;
+
+/**
+ *
+ * @author Detobel36
+ */
+public class ReadXML {
+    
+    // Example
+    //Ajouter cette méthodes à la classe JDOM2
+//    static void afficheALL() {
+//        //On crée une List contenant tous les noeuds "etudiant" de l'Element racine
+//        List listEtudiants = racine.getChildren("etudiant");
+//
+//        //On crée un Iterator sur notre liste
+//        Iterator i = listEtudiants.iterator();
+//        while(i.hasNext()) {
+//            //On recrée l'Element courant à chaque tour de boucle afin de
+//            //pouvoir utiliser les méthodes propres aux Element comme :
+//            //sélectionner un nœud fils, modifier du texte, etc...
+//            Element courant = (Element)i.next();
+//            //On affiche le nom de l’élément courant
+//            System.out.println(courant.getChild("nom").getText());
+//        }
+//    }
+    
+    
+}
