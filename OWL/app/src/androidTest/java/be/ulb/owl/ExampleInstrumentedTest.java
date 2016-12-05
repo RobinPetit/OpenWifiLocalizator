f9e58d98a1bdd2859e2a2a0d6cd74bbc70f1ef83
Detobel36 <detobel36@phoenix-rebirth.fr>
2016-12-05 08:42:53 +0100

    Move ULBMAP :arrow_right: OWL (and adapt gitignore)

--- OWL/app/src/androidTest/java/be/ulb/owl/ExampleInstrumentedTest.java
+++ OWL/app/src/androidTest/java/be/ulb/owl/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.ulbmap.ulbmap;
+package be.ulb.owl;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
--- OWL/app/src/test/java/be/ulb/owl/ExampleUnitTest.java
+++ OWL/app/src/test/java/be/ulb/owl/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.ulbmap.ulbmap;
+package be.ulb.owl;
 
 import org.junit.Test;
 
