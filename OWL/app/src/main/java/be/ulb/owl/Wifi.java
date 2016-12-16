aad1d4cbde1e8e3d4003e8e0c4a293634c5fa5ab
Detobel36 <detobel36@phoenix-rebirth.fr>
2016-12-16 11:22:10 +0100

    Remove unused method

--- OWL/app/src/main/java/be/ulb/owl/Wifi.java
+++ OWL/app/src/main/java/be/ulb/owl/Wifi.java
@@ -51,22 +51,7 @@ public class Wifi {
     public String getBSS() {
         return _BSS;
     }
-    
-    /**
-     * Check if the wifi have a good Likelyhood
-     * 
-     * @param wifi the wifi who must be compare
-     * @return the score of likelyhood (or 0 if BSS are not the same)
-     */
-    public int getLikelyhood(Wifi wifi) {
-        int res = 0;
-        if(_BSS.equals(wifi.getBSS())) {
-            // TODO compare wifi
-            // cc @RobinPetit
-        }
-        return res;
-    }
-    
+
 
     public boolean equals(Wifi wifi) {
         return _BSS.equals(wifi.getBSS());
