76aa7de2021caff233540631ffb879fb989d5dc2
RobinPetit <robpetit@ulb.ac.be>
2016-12-17 14:30:12 +0100

    [GraphMaker] Set Config.py back

---
+++ GraphMaker/app/Config.py
@@ -0,0 +1,10 @@
+class Config:
+
+    # Change this according to your network device id
+    NETWORK_INTERFACE = 'wlo1'
+    # Debug Mode
+    DEBUG = True
+    # MAPS location
+    MAPS_PATH = '../plans/'
+    # XMLs location
+    XMLS_PATH = '../XML maps/'
