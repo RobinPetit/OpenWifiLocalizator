be9358ee6fef8a5930bf14f251971213c63d4013
RobinPetit <robpetit@ulb.ac.be>
2017-02-19 23:40:42 +0100

    [splines] Writes documentation of Hermite spline class

--- OWL/app/src/main/java/be/ulb/owl/gui/pathdrawing/HermitianCubicSplinePathDrawer.java
+++ OWL/app/src/main/java/be/ulb/owl/gui/pathdrawing/HermitianCubicSplinePathDrawer.java
@@ -11,13 +11,47 @@ import be.ulb.owl.gui.FloatCouple;
  */
 
 public class HermitianCubicSplinePathDrawer extends PathDrawer {
-    static private int NB_POINTS_PER_SEGMENT = 50;
+    /**
+     * cubic curves are not drawn directly by a canvas function. Then the function must
+     * be evaluated in a given amount of points to be displayed in order to mimic such a curve.
+     */
+    static private int NB_POINTS_PER_SEGMENT = 25;
+
+    /**
+     * dimension of the interpolation curve (here 3 since curves are cubic)
+     */
     static private int DIMENSION = 3;
+
+    /**
+     * value to divide the derivatives by in order to add the required  constraint
+     * to determine the 4 parameters of each "sub-curve".
+     *
+     * See Hermite splines for more information about this.
+     */
     static private float DERIVATIVE_REGULARISATION_FACTOR = 2.f;
+
+    /**
+     * Array of coefficients for each monomial \alpha_{ik} * t^k and for each sub-curve
+     * joining two points to interpolate
+     */
     private FloatCouple[][] _alpha;
+
+    /**
+     * array of derivative values at each point. Derivative is given by a weighted average
+     * of next and previous node. Two additional constraints must be set for gamma[0] and gamma[-1]
+     */
     private FloatCouple[] _gamma;
+
+    /**
+     * The number of points (knots) to interpolate
+     */
     private int _N = 0;
 
+    /**
+     * Constructor
+     * @param pathColor color to draw the path with
+     * @param canvas canvas to draw the path on
+     */
     public HermitianCubicSplinePathDrawer(int pathColor, Canvas canvas) {
         super(pathColor, canvas);
     }
@@ -27,6 +61,11 @@ public class HermitianCubicSplinePathDrawer extends PathDrawer {
         // @TODO
     }
 
+    /**
+     * draws a path interpolating all of the given points in the correct order
+     * by piece-wise cubic curves
+     * @param points list of points to interpolate
+     */
     @Override
     public void drawPath(List<FloatCouple> points) {
         super.drawPath(points);
@@ -34,14 +73,19 @@ public class HermitianCubicSplinePathDrawer extends PathDrawer {
         setNumberOfPoints(N);
         _alpha = new FloatCouple[N][DIMENSION+1];
         _gamma = new FloatCouple[N];
-        computeDerivatives(points);
-        computeCoefficients(points);
+        computeDerivatives(points);  // get gamma
+        computeCoefficients(points);  // get alpha
         drawPath();
     }
 
+    /**
+     * Draws the path when all the coefficients have benn computed
+     */
     private void drawPath() {
         int N = getNumberOfPoints();
+        // for each sub curve
         for(int i = 0; i < N-1; ++i) {
+            // mimic a cubic curve by drawing several points of the cubic curve
             for(int step = 0; step < NB_POINTS_PER_SEGMENT+1; ++step) {
                 float x = evaluateX(i, step/(float)NB_POINTS_PER_SEGMENT);
                 float y = evaluateY(i, step/(float)NB_POINTS_PER_SEGMENT);
@@ -50,14 +94,26 @@ public class HermitianCubicSplinePathDrawer extends PathDrawer {
         }
     }
 
+    /**
+     * @link _N
+     * @param N the new amount of nodes that must be interpolated
+     */
     private void setNumberOfPoints(int N) {
         _N = N;
     }
 
+    /**
+     * get the amount of nodes to interpolate
+     * @return The number of knots
+     */
     protected int getNumberOfPoints() {
         return _N;
     }
 
+    /**
+     *
+     * @param points
+     */
     private void computeCoefficients(List<FloatCouple> points) {
         int N = getNumberOfPoints();
         for(int i = 0; i < N-1; ++i) {
@@ -71,6 +127,12 @@ public class HermitianCubicSplinePathDrawer extends PathDrawer {
         }
     }
 
+    /**
+     * Compute the constraints on the derivatives (for cubic hermitian splines, this constraint
+     * is given by \gamma_i = (p_{i+1} - p_{i-1})/R, with R the regularisation factor and p_i = (x_i, y_i)
+     * the ith point to interpolate)
+     * @param points The list of knots to interpolate
+     */
     private void computeDerivatives(List<FloatCouple> points) {
         setExtremaDerivatives(points);
         int N = getNumberOfPoints();
@@ -81,12 +143,24 @@ public class HermitianCubicSplinePathDrawer extends PathDrawer {
         }
     }
 
+    /**
+     * pretty arbitrary additional constraint on \gamma_0 and gamma_{N-1}
+     * @param points The list of points to interpolate
+     */
     protected void setExtremaDerivatives(List<FloatCouple> points) {
         int N = getNumberOfPoints();
+        // here, set the first derivative of the curve to be null at both extremities
+        // (theoretically, any value could fit here)
         _gamma[0] = new FloatCouple(.0f, .0f);
         _gamma[N-1] = new FloatCouple(.0f, .0f);
     }
 
+    /**
+     * evaluates the x-value of the interpolation function on given segment at given value
+     * @param idx The index representing the segment to evaluate the interpolation on
+     * @param t The value to evaluate at (\in [0, 1])
+     * @return The value of the interpolating function on segment idx at value t
+     */
     private float evaluateX(int idx, float t) {
         float[] xCoeff = new float[_alpha[idx].length];
         for(int i = 0; i < xCoeff.length; ++i) {
@@ -95,6 +169,12 @@ public class HermitianCubicSplinePathDrawer extends PathDrawer {
         return evaluatePolynomial(xCoeff, t);
     }
 
+    /**
+     * evaluates the y-value of the interpolation function on given segment at given value
+     * @param idx The index representing the segment to evaluate the interpolation on
+     * @param t The value to evaluate at (\in [0, 1])
+     * @return The value of the interpolating function on segment idx at value t
+     */
     private float evaluateY(int idx, float t) {
         float[] yCoeff = new float[_alpha[idx].length];
         for(int i = 0; i < yCoeff.length; ++i) {
@@ -103,6 +183,12 @@ public class HermitianCubicSplinePathDrawer extends PathDrawer {
         return evaluatePolynomial(yCoeff, t);
     }
 
+    /**
+     * Evaluates a generic polynomial determined by its coefficients at given value
+     * @param coeff Array of coefficients such that coeff[i] is the coefficient of t^i
+     * @param t the real value to evaluate the polynomial at
+     * @return The value taken by the polynomial at value t
+     */
     static private float evaluatePolynomial(final float[] coeff, float t) {
         float tPowered = 1;
         float ret = 0;
