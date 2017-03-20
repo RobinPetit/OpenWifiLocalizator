6fcf1ec5337a1ceb8f59abe940f728105caabec1
Detobel36 <detobel36@phoenix-rebirth.fr>
2017-03-20 18:24:55 +0100

    Fix #58 (back button make nothing ! :smiley: )

--- OWL/app/src/main/java/be/ulb/owl/MainActivity.java
+++ OWL/app/src/main/java/be/ulb/owl/MainActivity.java
@@ -281,7 +281,7 @@ public class MainActivity extends AppCompatActivity  {
             _searchView.closeSearch();
             displayPlan();
         } else {
-            super.onBackPressed();
+            //super.onBackPressed();
         }
     }
 
@@ -311,14 +311,7 @@ public class MainActivity extends AppCompatActivity  {
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-
+        setFullScreen();
     }
 
 
@@ -484,6 +477,21 @@ public class MainActivity extends AppCompatActivity  {
         matrixView.setTranslate(-xMove, -yMove);
 
         TouchListener.setNewCoordZoom(matrixView, _imageDraw, _imageView);
+    }
+
+    /**
+     * Set application on full screen IF debug mode is on
+     */
+    private void setFullScreen() {
+        if(isDemo()) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 
 
